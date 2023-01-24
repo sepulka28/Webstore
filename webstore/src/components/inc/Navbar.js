3d75b5685e57c8732148dd67541001e91cbd6b1e
Sepulka28 <ewelina.zaabik@gmail.com>
2023-01-25 00:42:36 +0100

    add button state change

--- webstore/src/components/inc/Navbar.js
+++ webstore/src/components/inc/Navbar.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
@@ -13,6 +15,16 @@ import Button from 'react-bootstrap/Button';
 
 
 function NavbarElement () {
+
+  const [buttonName, setButtonName] = useState('LOG IN');
+  let location = useLocation();
+
+  useEffect(() => {
+    if(location.pathname === '/dashboard'){
+      setButtonName("LOG OUT")
+    }
+    
+  }, [location.pathname]);
   
   return (
     <Navbar className="nav" expand="lg">
@@ -28,7 +40,7 @@ function NavbarElement () {
             navbarScroll
           >
             <Nav.Link href="/home" style={{ color: "#181819"}}>Home</Nav.Link>
-            {/* <Nav.Link href="/about" style={{ color: "#181819"}}>About</Nav.Link> */}
+            
             <NavDropdown title="Products" id="navbarScrollingDropdown">
               <NavDropdown.Item href="/action"></NavDropdown.Item>
               <NavDropdown.Item href="/action4">
@@ -66,7 +78,7 @@ function NavbarElement () {
           
           <span className="shopping_bag"><BsHandbag className='shop_bag_icon'/> SHOPPING BAG</span>
           <span className="favourite"><BsSuitHeart className='fav_icon'/> FAVOURITE</span>
-          <Button className="log_in_button" href="/login"><span><VscAccount className="log_in_icon"/> LOG IN</span></Button>
+          <Button className="log_in_button" href="/login"  onClick={() => setButtonName(buttonName)}><span><VscAccount className="log_in_icon"/> {buttonName}</span></Button>
          
           </div>
           
--- webstore/src/components/pages/Register.js
+++ webstore/src/components/pages/Register.js
@@ -2,21 +2,65 @@ import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container';
-// import { zodResolver } from "@hookform/resolvers/zod";
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useForm } from "react-hook-form";
-// import { ErrorMessage } from '@hookform/error-message';
-// import validateData from '../../model';
+import { useFormik } from 'formik';
+
+import * as Yup from 'yup';
+
+    const validateInput = values => {
+    const errors = {};
+
+    if(!values.name) {
+      errors.name = "This field is required"
+    } else if (values.name.length > 20){
+      errors.name = "Name can't exceed 20 characters"
+    }
+
+    if(!values.email) {
+      errors.email = "This field is required"
+    } 
+
+    if(!values.password) {
+      errors.password = "This field is required"
+    } else if (values.password.length < 8){
+      errors.password = "Password must be at least 8 characters"
+    }
+
+    return errors
+  }
 
 
 export default function Register () {
 
-  const { register, handleSubmit, formState: { errors } } = useForm()
+  const formik = useFormik({
+    initialValues: {
 
-  const onSubmit = data => console.log(data);
+      name: "",
+      email: "",
+      password: ""
+    },
 
-  console.log(errors)
+    validationSchema: Yup.object({
+      name: Yup.string()
+        .max(15, "Must be 15 characters or less")
+        .required("This field is required"),
+        
+      email: Yup.string().email("Invalid email address").required("This field is required"),
+
+      password: Yup.string()
+        .min(8, "Password must be at least 8 characters")
+        .max(20, "Password must be 20 characters or less")
+        .required("This field is required"),
+    }),
+
+    validateInput,
+    onSubmit: values => {
+      alert(JSON.stringify(values));
+    },
+    
+  });
+
 
   const navigate = useNavigate();
 
@@ -49,28 +93,32 @@ export default function Register () {
 
 
   return (
+    
     <div>
+      
       <Container fluid className="log_in_form">
-      <Form onSubmit={handleSubmit(onSubmit)}>
+      
+      <Form onSubmit={formik.handleSubmit}>
         <div className="title"><span className="sign_in_title">Sign Up Form</span></div>
       <Form.Group>
         <Form.Label >Name*</Form.Label>
-          <Form.Control placeholder="Enter your name" {...register("name", {required: true, minLength: 2, maxLength: 20, message: "This field is required"} )} className="name_label" onChange={(e) => setName( e.target.value )}/>
-         <p>{errors.name}</p>
+          <Form.Control placeholder="Enter your name" className="name_label" id = "name" value = {formik.values.name} onChange={(e) => setName( e.target.value )} onInput = {formik.handleChange}  onBlur={formik.handleBlur}/>
+          {formik.touched.name && formik.errors.name ? ( <p className = "error_paragraph">{formik.errors.name}</p> ) : null}
+         
       </Form.Group>
 
       <Form.Group>
         <Form.Label className="email_reg_label_text">Email*</Form.Label>
-        <Form.Control type="email" {...register("email", {required: true, message: "This field is required"} )} placeholder="Enter your email" className="email_reg_label" onChange={(e) => setEmail( e.target.value )} />
-        <p>{errors.name}</p>
+        <Form.Control type="email" placeholder="Enter your email" className="email_reg_label" id = "email" value = {formik.values.email} onChange={(e) => setEmail( e.target.value )} onInput = {formik.handleChange}  onBlur={formik.handleBlur}/>
+        {formik.touched.email && formik.errors.email ? ( <p className = "error_paragraph">{formik.errors.email}</p>  ) : null}
+        
       </Form.Group>
 
       <Form.Group>
         <Form.Label className="password_reg_label_text">Password*</Form.Label>
-        <Form.Control input type="password" 
-        {...register("password", {required: true, minLength:
-        {value: 8, message: "The password must contain at least 8 digits"} }) } placeholder="Enter your password" className="password_reg_label" onChange={(e) => setPassword( e.target.value )}/>
-         <p>{errors.name}</p>
+        <Form.Control input type="password" placeholder="Enter your password" className="password_reg_label" id = "password" value = {formik.values.password} onChange={(e) => setPassword( e.target.value )} onInput = {formik.handleChange} onBlur={formik.handleBlur}/>
+        {formik.touched.password && formik.errors.password ? ( <p className = "error_paragraph">{formik.errors.password}</p> ) : null}
+         
       </Form.Group>
      
       <Button type="submit" className="log_in_form_btn" onClick={ registerUser }>
@@ -80,9 +128,13 @@ export default function Register () {
       <div><p className="already_a_member">Already a member? <a href="./login"> Log in</a></p></div>
 
     </Form>
+    
     </Container>
     </div>
   );
   }
+ 
+
+
 
 
