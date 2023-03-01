6db74ebd666c6f9bf404dcde9b3d6e79d8a8e843
Sepulka28 <ewelina.zaabik@gmail.com>
2023-03-01 20:14:18 +0100

    add product descriptions and create favourites and shopping bag pages

--- webstore/src/components/pages/AC.js
+++ webstore/src/components/pages/AC.js
@@ -19,12 +19,12 @@ function Cards () {
 
   const products = [
 
-    { id: 1, title: "Product 1", image: Image4 },
-    { id: 2, title: "Product 2", image: Image5 },
-    { id: 3, title: "Product 3", image: Image6 },
-    { id: 4, title: "Product 4", image: Image7 },
-    { id: 5, title: "Product 5", image: Image8 },
-    { id: 6, title: "Product 6", image: Image9 }
+    { id: 1, title: "Product 1", image: Image4, description: "High-quality high chair for your little highness. Order now and enjoy free shipping!" },
+    { id: 2, title: "Product 2", image: Image5, description: "Elevate your space with our premium chair. Designed with style and comfort in mind. Order now and enjoy free shipping!" },
+    { id: 3, title: "Product 3", image: Image6, description: "Relax in luxury with our plush chair. Perfect for unwinding after a long day. Order now for free shipping!" },
+    { id: 4, title: "Product 4", image: Image7, description: "Upgrade your space with our sleek and contemporary chair. Combining style and comfort, it's perfect for any modern home." },
+    { id: 5, title: "Product 5", image: Image8, description: "Experience the perfect blend of style and functionality with our simple wooden chair. Order now and enjoy sturdy, comfortable seating with free shipping!" },
+    { id: 6, title: "Product 6", image: Image9, description: "Enhance your home decor with our selection of comfortable and stylish chairs. Enjoy free shipping on your purchase today!" }
     
   ]
 
@@ -65,10 +65,9 @@ function Cards () {
     <Card className="card_ac" style={{ width: '18rem' }}>
       <Card.Img variant="center" src={product.image} className="image" />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>{product.title}</Card.Title>
         <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+          {product.description}
         </Card.Text>
         <FaShoppingCart className='shop_bag_icon_card' onClick={() => addCartProduct(product)} style={{ color: cart.some((p) => p.id === product.id) ? "#3fa24f" : "black" }}/> <IoIosHeart className='favourite_icon_card' 
         onClick={() => addLikedProduct(product)} style={{ color: fav.some((p) => p.id === product.id) ? "red" : "black" }}/>
