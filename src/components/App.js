fef3c4540aaac632c27feed68e67f5300688b995
Sepulka28 <ewelina.zaabik@gmail.com>
2023-07-10 21:16:15 +0200

    correct the spelling in import statement

--- src/components/App.js
+++ src/components/App.js
@@ -12,7 +12,7 @@ import Login from "./pages/Login"
 import Register from "./pages/Register"
 import Dashboard from "./pages/Dashboard"
 import AC from "./pages/AC"
-import ShoppingBag from "./pages/shoppingbag";
+import ShoppingBag from "./pages/ShoppingBag";
 import Favourites from './pages/Favourites';
 import Tables from './pages/Tables';
 import Dressers from './pages/Dressers';
