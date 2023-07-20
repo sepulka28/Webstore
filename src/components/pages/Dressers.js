fea7a1953a704f424bcd24474031ea1911b9bc04
Sepulka28 <ewelina.zaabik@gmail.com>
2023-07-20 21:19:21 +0200

    remove and modify certain files to simplify the project

--- src/components/pages/Dressers.js
+++ src/components/pages/Dressers.js
@@ -1,48 +1,10 @@
 import React from 'react';
-import { useState } from 'react';
 import Card from 'react-bootstrap/Card';
-
-import { FaShoppingCart } from "react-icons/fa";
-import { IoIosHeart } from "react-icons/io";
 
 import { Dressers } from '../ProductData';
 
 function Dresser () {
   
-  const [cart, setCart] = useState([]);
-  const [fav, setFav] = useState([]);
-
-  const addCartProduct = product => {
-    const index = cart.findIndex(p => p.id === product.id);
-    if (index === -1) {
-      setCart([...cart, { ...product }]);
-      const existingProducts = localStorage.getItem('products');
-      const newProduct = { id: product.id, title: product.title, image: product.images };
-      if (!existingProducts) {
-        localStorage.setItem('products', JSON.stringify([newProduct]));
-      }
-    } else {
-      setCart(cart.filter(p => p.id !== product.id));
-      localStorage.removeItem('products');
-    }
-  };
-
-  
-  const addLikedProduct = product => {
-    const index = fav.findIndex(p => p.id === product.id);
-    if (index === -1) {
-      setFav([...fav, { ...product }]);
-      const existingFav = localStorage.getItem('fav');
-      const newFav = { id: product.id, title: product.title };
-      if (!existingFav) {
-        localStorage.setItem('products', JSON.stringify([newFav]));
-      }
-    } else {
-      setFav(fav.filter(p => p.id !== product.id));
-      localStorage.removeItem('products');
-    }
-  };
-
   return (
     <div className="ac_card_container container-fluid " >
 
@@ -55,8 +17,6 @@ function Dresser () {
         <Card.Text>
           {dresser.description}
         </Card.Text>
-        <FaShoppingCart className='shop_bag_icon_card' onClick={() => addCartProduct(dresser)} style={{ color: cart.some((p) => p.id === dresser.id) ? "#3fa24f" : "black" }}/> <IoIosHeart className='favourite_icon_card' 
-        onClick={() => addLikedProduct(dresser)} style={{ color: fav.some((p) => p.id === dresser.id) ? "red" : "black" }}/>
       </Card.Body>
     </Card>
     </div>
--- src/components/pages/Sofas.js
+++ src/components/pages/Sofas.js
@@ -1,47 +1,10 @@
 import React from 'react';
-import { useState } from 'react';
 import Card from 'react-bootstrap/Card';
 
 import { Sofas } from '../ProductData';
 
-import { FaShoppingCart } from "react-icons/fa";
-import { IoIosHeart } from "react-icons/io";
-
 function Sofa () {
-  
-  const [cart, setCart] = useState([]);
-  const [fav, setFav] = useState([]);
 
-
-  const addCartProduct = product => {
-    const index = cart.findIndex(p => p.id === product.id);
-    if (index === -1) {
-      setCart([...cart, { ...product }]);
-      const existingProducts = localStorage.getItem('products');
-      const newProduct = { id: product.id, title: product.title, image: product.images };
-      if (!existingProducts) {
-        localStorage.setItem('products', JSON.stringify([newProduct]));
-      }
-    } else {
-      setCart(cart.filter(p => p.id !== product.id));
-      localStorage.removeItem('products');
-    }
-  };
-  
-  const addLikedProduct = product => {
-    const index = fav.findIndex(p => p.id === product.id);
-    if (index === -1) {
-      setFav([...fav, { ...product }]);
-      const existingFav = localStorage.getItem('fav');
-      const newFav = { id: product.id, title: product.title };
-      if (!existingFav) {
-        localStorage.setItem('products', JSON.stringify([newFav]));
-      }
-    } else {
-      setFav(fav.filter(p => p.id !== product.id));
-      localStorage.removeItem('products');
-    }
-  };
 
   return (
     <div className="ac_card_container container-fluid " >
@@ -54,8 +17,6 @@ function Sofa () {
         <Card.Text>
           {sofa.description}
         </Card.Text>
-        <FaShoppingCart className='shop_bag_icon_card' onClick={() => addCartProduct(sofa)} style={{ color: cart.some((p) => p.id === sofa.id) ? "#3fa24f" : "black" }}/> <IoIosHeart className='favourite_icon_card' 
-        onClick={() => addLikedProduct(sofa)} style={{ color: fav.some((p) => p.id === sofa.id) ? "red" : "black" }}/>
       </Card.Body>
     </Card>
     </div>
